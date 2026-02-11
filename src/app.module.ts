51ddbb6f1474df83cb42f6fbb66b768dfe76074a
Muhammad Chiko Kulafaur Rozikin <chiko@gmail.com>
2026-02-11 08:33:07 +0700

    prepare deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,8 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath:
-        process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
     }),
     PrismaModule,
     BooksModule,
